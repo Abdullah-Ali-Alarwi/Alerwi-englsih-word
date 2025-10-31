ab8532754d364d9582e4bd719b413d3d412177b3
Abdullah-Ali-Alariw <alarwi.pro@gmail.com>
2025-10-31 20:36:40 +0300

    دمج store وملفات المفضلة، إضافة getFavorites وremoveFromFavorites

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar" dir="rtl">
-      <body className="bg-gray-900 text-white min-h-screen flex flex-col mt-5">
+      <body className="bg-gray-900 text-white min-h-screen flex flex-col mt-10">
         
         {/* ✅ الهيدر الآن يستخدم Zustand لتحديث البيانات مباشرة */}
         <Header />
